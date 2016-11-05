8518f3165f97a1702d375fa9887fce0cc8769043
nkleemann <n.kleemann@openmailbox.org>
2016-11-05 21:55:32 +0100

    Update fib.cpp

--- c++/fib.cpp
+++ c++/fib.cpp
@@ -4,7 +4,7 @@
 int main (int argc, char **argv)
 {
 	unsigned int fib = 0, tmp = 1;
-
+  
   for (unsigned int i = atoi(argv[1]); i-- && i < 47;)
 	{
 	   printf("%i%c", fib, i?' ':'\n'); 									//toll, oder martin?
