12396177a8bb180f994e72b92a8d281d389749ce
nkleemann <n.kleemann@openmailbox.org>
2016-11-05 22:01:00 +0100

    tabs vs spaces

--- c++/fib.cpp
+++ c++/fib.cpp
@@ -4,20 +4,11 @@
 int main (int argc, char **argv)
 {
 	unsigned int fib = 0, tmp = 1;
-<<<<<<< HEAD
 
 	for (unsigned int i = atoi(argv[1]); i-- && i < 47;)
 	{
-	printf("%i%c", fib, i?' ':'\n'); //toll, oder martin?
+	printf("%i%c", fib, i?' ':'\n'); 											//toll, oder martin?
 	fib += tmp;
 	tmp  = fib - tmp;
-=======
-  
-  for (unsigned int i = atoi(argv[1]); i-- && i < 47;)
-	{
-	   printf("%i%c", fib, i?' ':'\n'); 									//toll, oder martin?
-   	 fib += tmp;
-     tmp  = fib - tmp;
->>>>>>> 8518f3165f97a1702d375fa9887fce0cc8769043
 	}
 }
